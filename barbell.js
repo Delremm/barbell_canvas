ffa2ecc2677551a4c1710bbea24b8d0b8159321b
delremm <g1m5yv@gmail.com>
2014-12-20 20:12:53 +0500

    text WxReps added

--- barbell.js
+++ barbell.js
@@ -31,42 +31,54 @@ function init() {
 	};
 	var drow_plates = function(c_w, c_h, ctx, plates){
 		var between_plates = 1/200.0;
-		var left_p = c_w/4;
-		var right_p = c_w*3/4;
+		var left_p = c_w*5/16;
+		var right_p = c_w*11/16;
 		var plates_shift = 0.0;
-		console.log(plates, "plates");
+		var plates_shift_color = "#ffffff";
 		for (var k in plates){
-				for (var i = 0;i<plates[k].num;i++){
-					// console.log("plate", plates[k]);
-					ctx.fillStyle = plates[k].color;
-					console.log(plates_shift);
-					console.log(c_w*plates[k].rel_w, "width");
-					// ctx.fillRect(left_p-plates_shift, c_h,c_w*plates[k].rel_w,c_h);
-					plates_shift += c_w*plates[k].rel_w;
-					ctx.fillRect(left_p-plates_shift,c_h*(1-plates[k].rel_h)/2, plates[k].rel_w*c_w,plates[k].rel_h*c_h/2.0);
-					ctx.fillRect(left_p-plates_shift,c_h/2.0,plates[k].rel_w*c_w,plates[k].rel_h*c_h/2.0);
-					console.log(plates[k].rel_h*c_h/2.0, "h/2");
-					ctx.fillStyle = "#ffffff";
-					plates_shift += between_plates*c_w;
-					ctx.fillRect(left_p-plates_shift,0,between_plates*c_w,plates[k].rel_h*c_h);
-4				};
+			for (var i = 0;i<plates[k].num;i++){
+				ctx.fillStyle = plates[k].color;
+				plates_shift += c_w*plates[k].rel_w;
+				ctx.fillRect(left_p-plates_shift,c_h*(1-plates[k].rel_h)/2, plates[k].rel_w*c_w,plates[k].rel_h*c_h/2.0);
+				ctx.fillRect(left_p-plates_shift,c_h/2.0,plates[k].rel_w*c_w,plates[k].rel_h*c_h/2.0);
+				ctx.fillRect(right_p+plates_shift,c_h*(1-plates[k].rel_h)/2, plates[k].rel_w*c_w,plates[k].rel_h*c_h/2.0);
+				ctx.fillRect(right_p+plates_shift,c_h/2.0,plates[k].rel_w*c_w,plates[k].rel_h*c_h/2.0);
+				ctx.fillStyle = plates_shift_color;
+				plates_shift += between_plates*c_w;
+				ctx.fillRect(left_p-plates_shift,0,between_plates*c_w,plates[k].rel_h*c_h);
+			};
 		};
-		// ctx.fillStyle = "#ff0000";
-		// ctx.fillRect(0,0,30,30);
 	};
 	var cs = document.getElementsByClassName("barbell");
 	for (var key in cs){
 		var c = cs[key];
-		var c_w = c.getAttribute('width');
 		var c_h = c.getAttribute('height');
-
+		var c_w = c.getAttribute('width');
 		var w = c.getAttribute('weight');
+		w = parseFloat(w).toFixed(1);
+		var reps = c.getAttribute('reps');
+		var unit = c.getAttribute('unit');
+		var u = 'kg';
+		if (unit==='lb'){
+			u = 'lb';
+		};
 		var plates = form_plates(w);
 
 		var ctx = c.getContext("2d");
-		ctx.fillStyle = "#999999";
-		var bar_thickness = c_h/10;
-		ctx.fillRect(0,c_h/2-bar_thickness/2,c_w,bar_thickness);
-		drow_plates(c_w, c_h, ctx, plates);
+		if ((w<450)&&(reps<999)){		
+			//drow a barbell
+			ctx.fillStyle = "#999999";
+			var bar_thickness = c_h/10;
+			ctx.fillRect(0,c_h/2-bar_thickness/2,c_w,bar_thickness);
+			drow_plates(c_w, c_h, ctx, plates);
+		}
+		//drow text
+		var font_size = c_h/3;
+		ctx.font = font_size + "px serif";
+		ctx.fillStyle = "#000";
+		ctx.fillText(w+''+u, c_w*1/3, c_h*1/3);
+		if (reps){
+			ctx.fillText('x'+reps, c_w*1/3, c_h*6/7);
+		};
 	}
 }
